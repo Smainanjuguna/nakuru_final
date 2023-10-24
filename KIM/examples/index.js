fa0e42bd11f6f06a665182360d2f6f9e5ce14184
Smainanjuguna <129868900+Smainanjuguna@users.noreply.github.com>
2023-10-24 15:33:49 +0300

    Add files via upload

---
+++ KIM/examples/index.js
@@ -0,0 +1,25 @@
+var map = L.map('map');
+
+// L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+// 	attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+// }).addTo(map);
+
+var control = L.Routing.control(L.extend(window.lrmConfig, {
+	waypoints: [
+		L.latLng(-0.2797631224182517, 36.044866764878094),
+		L.latLng(-0.28286918538826417, 36.16854641145356)
+	],
+	geocoder: L.Control.Geocoder.nominatim(),
+	routeWhileDragging: true,
+	reverseWaypoints: true,
+	showAlternatives: true,
+	altLineOptions: {
+		styles: [
+			{color: 'black', opacity: 0.15, weight: 9},
+			{color: 'white', opacity: 0.8, weight: 6},
+			{color: 'blue', opacity: 0.5, weight: 2}
+		]
+	}
+})).addTo(map);
+
+L.Routing.errorControl(control).addTo(map);
